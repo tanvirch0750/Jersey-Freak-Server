a424c65890a59ce2e0bda7f46d4afd1c7525dcf9
tanvirch0750 <tanvir0750@yahoo.com>
2022-07-14 21:11:59 +0600

    connect to mongodb

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-console.log('hello');
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+mongoose
+  .connect(
+    `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.quhih.mongodb.net/?retryWrites=true&w=majority`
+  )
+  .then(() => console.log('db connection successfull'))
+  .catch((err) => console.log(console.log(err)));
+
+app.listen(5000, () => {
+  console.log('Backend server is running');
+});
