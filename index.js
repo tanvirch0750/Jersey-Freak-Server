eefeae8bd461225d7e600fb97140d1d0d15c29c1
tanvirch0750 <tanvir0750@yahoo.com>
2022-07-17 23:36:42 +0600

    implemented stripe chekout route

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const authRoute = require('./routes/auth');
 const productRoute = require('./routes/product');
 const cartRoute = require('./routes/cart');
 const orderRoute = require('./routes/order');
+const stripeRoute = require('./routes/stripe');
 
 app.use(cors());
 app.use(express.json());
@@ -24,6 +25,7 @@ app.use('/api/users', userRoute);
 app.use('/api/products', productRoute);
 app.use('/api/carts', cartRoute);
 app.use('/api/orders', orderRoute);
+app.use('/api/checkout', stripeRoute);
 
 app.listen(5000, () => {
   console.log('Backend server is running');
